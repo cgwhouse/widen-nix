21291822b9074e5b9c6029a990fd59b5caa6e1bb
Cristian Widenhouse <cgwidenhouse@gmail.com>
2026-05-07 16:22:28 -0400

    Finish getting git-credential-manager working

--- home.nix
+++ home.nix
@@ -49,9 +49,17 @@
 
   programs.git = {
     enable = true;
-    settings.user = {
-      name = "Cristian Widenhouse";
-      email = "cgwidenhouse@gmail.com";
+    settings = {
+      user = {
+        name = "Cristian Widenhouse";
+        email = "cgwidenhouse@gmail.com";
+      };
+
+      credential = {
+        credentialStore = "cache";
+        helper = "manager";
+        "https://github.com".username = "cgwhouse";
+      };
     };
   };
 
