3ab67c06c73bf1e589e93df1d2abd517d10ba88f
Cristian Widenhouse <cristian.widenhouse@moovila.com>
2026-05-08 10:53:09 -0400

    WIP

--- home/cristian/vscode.nix
+++ home/cristian/vscode.nix
@@ -1,5 +1,11 @@
 { pkgs, osConfig, ... }:
 
+let
+  # Path to the widen-nix repo, relative to $HOME
+  # NOTE: This does require that this repo be at a well-known path, but this
+  # is standard convention for me, so will do it to satisfy VS Code
+  widenNixPath = "repos/widen-nix";
+in
 {
   programs.vscode = {
     enable = true;
@@ -8,18 +14,24 @@
         jnoortheen.nix-ide
       ];
 
-      # TODO: build out userSettings
       userSettings = {
-        "files.autoSave" = "off";
-
         "nix.enableLanguageServer" = true;
         "nix.serverPath" = "nixd";
         "nix.serverSettings" = {
-          nixd = {
-            formatting.command = [ "nixfmt" ];
-            options.nixos.expr = ''(builtins.getFlake "''${workspaceFolder}").nixosConfigurations.${osConfig.networking.hostName}.options'';
-          };
+          nixd.formatting.command = [ "nixfmt" ];
         };
+      };
+    };
+  };
+
+  # Workspace-scoped VS Code settings for this repo. Note that there is a bit
+  # of duplication compared to the user-scoped settings above, to prevent VS Code
+  # from clobbering the user settings with the workspace settings
+  home.file."${widenNixPath}/.vscode/settings.json".text = builtins.toJSON {
+    "nix.serverSettings" = {
+      nixd = {
+        formatting.command = [ "nixfmt" ];
+        options.nixos.expr = ''(builtins.getFlake "''${workspaceFolder}").nixosConfigurations.${osConfig.networking.hostName}.options'';
       };
     };
   };
