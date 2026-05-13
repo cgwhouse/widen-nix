4de2dd26b781407024d07a2ccf50a01ef82f3b4b
Cristian Widenhouse <cristian.widenhouse@moovila.com>
2026-05-13 16:45:58 -0400

    WIP

--- modules/nixos/catppuccin.nix
+++ modules/nixos/catppuccin.nix
@@ -1,3 +1,5 @@
+{ config, lib, ... }:
+
 {
   catppuccin.sddm = {
     enable = true;
@@ -10,4 +12,19 @@
     enable = true;
     flavor = "mocha";
   };
+
+  # Upstream catppuccin/nix bug: the `.plymouth` files ship with a hardcoded
+  # ImageDir=/usr/share/... that doesn't exist on NixOS, so plymouth renders
+  # nothing. Patch the installed files post-build to point at $out.
+  boot.plymouth.themePackages = lib.mkForce [
+    (config.catppuccin.sources.plymouth.overrideAttrs (old: {
+      postInstall = (old.postInstall or "") + ''
+        for d in "$out"/share/plymouth/themes/*/; do
+          name=$(basename "$d")
+          substituteInPlace "$d$name.plymouth" \
+            --replace-fail '/usr/share/plymouth/themes' "$out/share/plymouth/themes"
+        done
+      '';
+    }))
+  ];
 }
