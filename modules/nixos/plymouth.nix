c924f486d9a9333836fa19d50888ff1aba836a5b
Cristian Widenhouse <cristian.widenhouse@moovila.com>
2026-05-13 16:25:57 -0400

    WIP

--- modules/nixos/plymouth.nix
+++ modules/nixos/plymouth.nix
@@ -4,6 +4,10 @@
     consoleLogLevel = 0;
     initrd.verbose = false;
 
+    # systemd-in-initrd lets plymouth start as early as possible,
+    # before any kernel/initrd text would flash on screen.
+    initrd.systemd.enable = true;
+
     kernelParams = [
       "quiet"
       "splash"
