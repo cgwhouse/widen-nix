57123655696c30c5597c491dd133a1c9bd5af6e4
Cristian Widenhouse <cgwidenhouse@gmail.com>
2026-05-13 15:02:27 -0400

    WIP

--- home/cristian/nix-index.nix
+++ home/cristian/nix-index.nix
@@ -2,12 +2,11 @@
   # Better error message when a command isn't on PATH
   programs = {
 
-  command-not-found.enable = false;
-nix-index = {
-    enable = true;
-    enableZshIntegration = true;
+    command-not-found.enable = false;
+    nix-index = {
+      enable = true;
+      enableZshIntegration = true;
+    };
   };
-  };
-  
 
 }
