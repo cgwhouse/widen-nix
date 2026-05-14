c9d96cb2772e78c27e5c57b9bc6ee804ece7831f
Cristian Widenhouse <cristian.widenhouse@moovila.com>
2026-05-14 11:34:45 -0400

    Phase 1

--- flake.nix
+++ flake.nix
@@ -5,6 +5,11 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
     catppuccin.url = "github:catppuccin/nix";
 
+    flake-parts = {
+      url = "github:hercules-ci/flake-parts";
+      inputs.nixpkgs-lib.follows = "nixpkgs";
+    };
+
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -24,46 +29,53 @@
 
   outputs =
     inputs@{
+      flake-parts,
       nixpkgs,
       catppuccin,
       home-manager,
       ...
     }:
-    let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" ];
 
-      mkHost =
-        dir:
-        nixpkgs.lib.nixosSystem {
-          inherit system;
-          specialArgs = { inherit inputs; };
-          modules = [
-            ./hosts/${dir}
-            catppuccin.nixosModules.catppuccin
-
-            home-manager.nixosModules.home-manager
-            {
-              home-manager.useGlobalPkgs = true;
-              home-manager.useUserPackages = true;
-              home-manager.extraSpecialArgs = { inherit inputs; };
-              home-manager.sharedModules = [ inputs.plasma-manager.homeModules.plasma-manager ];
-
-              home-manager.users.cristian = {
-                imports = [
-                  ./home/cristian
-                  catppuccin.homeModules.catppuccin
-                ];
-              };
-            }
-          ];
+      perSystem =
+        { pkgs, ... }:
+        {
+          formatter = pkgs.nixfmt;
         };
-    in
-    {
-      nixosConfigurations = {
-        widen-nix-vm = mkHost "vm";
-      };
 
-      formatter.${system} = pkgs.nixfmt;
+      flake =
+        let
+          mkHost =
+            dir:
+            nixpkgs.lib.nixosSystem {
+              system = "x86_64-linux";
+              specialArgs = { inherit inputs; };
+              modules = [
+                ./hosts/${dir}
+                catppuccin.nixosModules.catppuccin
+
+                home-manager.nixosModules.home-manager
+                {
+                  home-manager.useGlobalPkgs = true;
+                  home-manager.useUserPackages = true;
+                  home-manager.extraSpecialArgs = { inherit inputs; };
+                  home-manager.sharedModules = [ inputs.plasma-manager.homeModules.plasma-manager ];
+
+                  home-manager.users.cristian = {
+                    imports = [
+                      ./home/cristian
+                      catppuccin.homeModules.catppuccin
+                    ];
+                  };
+                }
+              ];
+            };
+        in
+        {
+          nixosConfigurations = {
+            widen-nix-vm = mkHost "vm";
+          };
+        };
     };
 }
