756278ef5fb19811d44816f144c1fce3ffb67f7c
Cristian Widenhouse <cgwidenhouse@gmail.com>
2026-05-15 13:41:00 -0400

    WIP

--- modules/flake/agenix.nix
+++ modules/flake/agenix.nix
@@ -3,8 +3,9 @@
 {
   flake.nixosModules.agenix =
     { pkgs, ... }:
+
     {
       imports = [ inputs.agenix.nixosModules.default ];
-      environment.systemPackages = [ inputs.agenix.packages.${pkgs.system}.default ];
+      environment.systemPackages = [ inputs.agenix.packages.${pkgs.stdenv.hostPlatform.system}.default ];
     };
 }
