f083a9fae9d7a3a9b0b1f65587753133f48c91a6
Cristian Widenhouse <cgwidenhouse@gmail.com>
2026-05-26 15:12:03 -0400

    Docker log rotation

--- modules/nixos/docker.nix
+++ modules/nixos/docker.nix
@@ -2,21 +2,13 @@
   virtualisation.docker = {
     enable = true;
 
-
-daemon.settings = 
-{
-  "log-driver" = "json-file";
-
-  "log-opts" = {
-    "max-size"= "10m";
-    "max-file" = "3";
-  };
-};
-    # daemon.settings = {
-    #   pruning = {
-    #     enabled = true;
-    #     interval = "24h";
-    #   };
-    # };
+    daemon.settings = {
+      # Log rotation
+      "log-driver" = "json-file";
+      "log-opts" = {
+        "max-size" = "10m";
+        "max-file" = "3";
+      };
+    };
   };
 }
