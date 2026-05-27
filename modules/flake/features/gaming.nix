025158288063a6ce7b63c3994affc1da88e48c2c
Cristian Widenhouse <cristian.widenhouse@moovila.com>
2026-05-27 16:08:53 -0400

    WIP

--- modules/flake/features/gaming.nix
+++ modules/flake/features/gaming.nix
@@ -1,6 +1,12 @@
 {
   flake.nixosModules.gaming = {
+    { pkgs, ... }:
+
     programs.steam.enable = true;
     programs.gamemode.enable = true;
+    
+home.packages = with pkgs; [
+       protonplus 
+      ];
   };
 }
