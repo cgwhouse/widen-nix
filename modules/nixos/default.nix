68ce7a2c005b8b69dc135b136598ba09555feba0
Cristian Widenhouse <cgwidenhouse@gmail.com>
2026-06-15 16:48:00 -0400

    Remove everything except secrets and profile pic

--- modules/nixos/default.nix
+++
@@ -1,9 +0,0 @@
-{ lib, ... }:
-
-let
-  isModule = name: type: type == "regular" && lib.hasSuffix ".nix" name && name != "default.nix";
-  moduleNames = builtins.attrNames (lib.filterAttrs isModule (builtins.readDir ./.));
-in
-{
-  imports = map (n: ./. + "/${n}") moduleNames;
-}
--- modules/nixos/firmware.nix
+++
@@ -1,4 +0,0 @@
-{
-  hardware.enableAllFirmware = true;
-  services.fwupd.enable = true;
-}
