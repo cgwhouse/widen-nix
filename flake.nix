3c42fb50ecc99103ef6ca119966ab9f62a81ce5f
Cristian Widenhouse <cgwidenhouse@gmail.com>
2026-06-16 16:55:37 -0400

    first attempt at adding home manager

--- flake.nix
+++ flake.nix
@@ -1,15 +1,36 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs =
-    { self, nixpkgs }@inputs:
+    inputs@{ nixpkgs, home-manager, ... }:
 
     {
-      nixosConfigurations.widen-nix-vm = nixpkgs.lib.nixosSystem {
-        specialArgs = { inherit inputs; };
-        modules = [ ./hosts/vm/default.nix ];
+      nixosConfigurations = {
+
+        widen-nix-vm = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          specialArgs = { inherit inputs; };
+
+          modules = [
+            ./hosts/vm/default.nix
+
+            home-manager.nixosModules.home-manager
+            {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.extraSpecialArgs = { inherit inputs; };
+              home-manager.users.cristian = ./home.nix;
+            }
+          ];
+        };
+
       };
     };
 }
