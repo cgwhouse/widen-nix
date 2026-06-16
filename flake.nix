7bd4602cdaa2593ceeefa16856ad7d35dab681f2
Cristian Widenhouse <cgwidenhouse@gmail.com>
2026-06-16 16:05:10 -0400

    fix flake.nix, add lockfile

--- flake.nix
+++ flake.nix
@@ -2,9 +2,8 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
   };
-  outputs = { self, nixpkgs }: {
+  outputs = { self, nixpkgs }@inputs: {
     nixosConfigurations.widen-nix-vm = nixpkgs.lib.nixosSystem {
-      system = "x86_64-linux";
       specialArgs = { inherit inputs; };
       modules = [ ./configuration.nix ];
     };
