78a1daec435c99552a6c91395c794d88ef365a5f
Cristian Widenhouse <cgwidenhouse@gmail.com>
2026-06-18 17:04:32 -0400

    add discord flatpak as a test

--- modules/flatpak.nix
+++ modules/flatpak.nix
@@ -1,3 +1,11 @@
 {
   services.flatpak.enable = true;
+
+  #flatpak install flathub com.discordapp.Discord
+
+  services.flatpak.packages = [
+    #{ appId = "com.brave.Browser"; origin = "flathub";  }
+    "com.discordapp.Discord"
+    #"im.riot.Riot"
+  ];
 }
