ca910fb2fa67bc8a96bc4354a7ffa404f7680b63
Cristian Widenhouse <cgwidenhouse@gmail.com>
2026-06-18 15:22:49 -0400

    Finish breaking down the contents of original configuration.nix from install

--- modules/default.nix
+++ modules/default.nix
@@ -6,29 +6,11 @@
     ./boot.nix
     ./fonts.nix
     ./locale.nix
+    ./networking.nix
     ./nix-settings.nix
     ./packages.nix
+    ./plasma.nix
     ./shell.nix
     ./users.nix
   ];
-
-  # Enable networking
-  networking.networkmanager.enable = true;
-
-  # Enable the X11 windowing system.
-  # You can disable this if you're only using the Wayland session.
-  services.xserver.enable = true;
-
-  # Enable the KDE Plasma Desktop Environment.
-  services.displayManager.sddm.enable = true;
-  services.desktopManager.plasma6.enable = true;
-
-  # Configure keymap in X11
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "";
-  };
-
-  # Enable CUPS to print documents.
-  services.printing.enable = true;
 }
---
+++ modules/plasma.nix
@@ -0,0 +1,10 @@
+{
+  services = {
+    # Disable X11
+    xserver.enable = false;
+
+    displayManager.sddm.enable = true;
+    desktopManager.plasma6.enable = true;
+  };
+
+}
