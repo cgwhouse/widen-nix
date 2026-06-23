400146909f3409611f679aa80d2bbfe37efdc215
Cristian Widenhouse <cgwidenhouse@gmail.com>
2026-06-23 16:54:58 -0400

    Re-key secret with new vm on this branch

--- secrets.nix
+++ secrets.nix
@@ -1,16 +1,10 @@
 let
-  vm-host = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAeDRamBXD78beSvm0KTKSoOaqHUu54qMw+SFusmUb38 root@widen-nix-vm";
-  vm-cristian = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHCbpu3fGG4TJobHZ57YA/ykqMxLp32sU+rM2mUs0bZh cristian@widen-nix-vm";
-
-  vm2-host = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHYJb48NNm9pQxyV4EHaSESYqeNnjdwVHveLBSihaECS root@widen-nix-vm";
-  vm2-cristian = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIErAEBoTq0DfxlO+EnkPTZ4jv3I7tcPY789CcvZbSg5x cristian@widen-nix-vm";
-
+  vm-host = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHYJb48NNm9pQxyV4EHaSESYqeNnjdwVHveLBSihaECS root@widen-nix-vm";
+  vm-cristian = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIErAEBoTq0DfxlO+EnkPTZ4jv3I7tcPY789CcvZbSg5x cristian@widen-nix-vm";
 
   all = [
     vm-host
     vm-cristian
-    vm2-host
-    vm2-cristian
   ];
 in
 {
