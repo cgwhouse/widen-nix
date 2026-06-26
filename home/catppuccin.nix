b61674ef0df4a92d2636a50012dfc86e2d21d363
Cristian Widenhouse <cgwidenhouse@gmail.com>
2026-06-26 14:14:55 -0400

    Add catppuccin globally on the home-manager side of things

---
+++ home/catppuccin.nix
@@ -0,0 +1,26 @@
+{ pkgs, ... }:
+
+{
+  imports = [ inputs.catppuccin.homeModules.catppuccin ];
+
+  catppuccin = {
+    enable = true;
+    autoEnable = true;
+    flavor = "mocha";
+    accent = "green";
+
+    # Sets home.pointerCursor (name + package) to the catppuccin cursor,
+    # niri reads the theme name from there
+    cursors.enable = true;
+  };
+
+  # pointerCursor.size has no default
+  home.pointerCursor.size = 24;
+
+  home.packages = with pkgs; [
+    (catppuccin-kde.override {
+      flavour = [ "mocha" ];
+      accents = [ "green" ];
+    })
+  ];
+}
--- home/default.nix
+++ home/default.nix
@@ -1,5 +1,6 @@
 {
   imports = [
+    ./catppuccin.nix
     ./chromium.nix
     ./direnv.nix
     ./firefox.nix
