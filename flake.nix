db3c859ab42711791d1ad5958a0c442346f47a66
Cristian Widenhouse <cgwidenhouse@gmail.com>
2026-06-26 14:06:47 -0400

    Add catppuccin flake

--- flake.nix
+++ flake.nix
@@ -2,6 +2,7 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nix-flatpak.url = "github:gmodena/nix-flatpak/latest";
+    catppuccin.url = "github:catppuccin/nix";
 
     home-manager = {
       url = "github:nix-community/home-manager";
@@ -30,6 +31,7 @@
       nixpkgs,
       home-manager,
       nix-flatpak,
+      catppuccin,
       ...
     }:
 
@@ -39,17 +41,22 @@
         widen-nix-vm = nixpkgs.lib.nixosSystem {
           system = "x86_64-linux";
           specialArgs = { inherit inputs; };
-
           modules = [
             ./hosts/vm/default.nix
             nix-flatpak.nixosModules.nix-flatpak
+            catppuccin.nixosModules.catppuccin
 
             home-manager.nixosModules.home-manager
             {
               home-manager.useGlobalPkgs = true;
               home-manager.useUserPackages = true;
               home-manager.extraSpecialArgs = { inherit inputs; };
-              home-manager.users.cristian = ./home/default.nix;
+              home-manager.users.cristian = {
+                imports = [
+                  ./home/default.nix
+                  catppuccin.homeModules.catppuccin
+                ];
+              };
             }
           ];
         };
