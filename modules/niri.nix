742a74b73420872d3d6488bc378d70fb60969f58
Cristian Widenhouse <cgwidenhouse@gmail.com>
2026-07-28 13:32:29 -0400

    Fix niri build issue

--- modules/niri.nix
+++ modules/niri.nix
@@ -1,6 +1,25 @@
+{ options, pkgs, ... }:
+
 {
   programs = {
-    niri.enable = true;
+    niri = {
+      enable = true;
+
+      # Temp workaround for libdisplay-info dependency issue
+      package = options.programs.niri.package.default.override {
+        libdisplay-info = pkgs.libdisplay-info.overrideAttrs (finalAttrs: {
+          version = "0.3.0";
+          src = pkgs.fetchFromGitLab {
+            domain = "gitlab.freedesktop.org";
+            owner = "emersion";
+            repo = "libdisplay-info";
+            rev = finalAttrs.version;
+            hash = "sha256-nXf2KGovNKvcchlHlzKBkAOeySMJXgxMpbi5z9gLrdc=";
+          };
+        });
+      };
+    };
+
     dms-shell.enable = true;
   };
 
