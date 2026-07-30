e719855b5f7ad361a723f5240d5917ff2482528f
Cristian Widenhouse <cgwidenhouse@gmail.com>
2026-07-30 09:15:44 -0400

    Remove temp workaround, update lockfile

--- modules/niri.nix
+++ modules/niri.nix
@@ -1,25 +1,6 @@
-{ options, pkgs, ... }:
-
 {
   programs = {
-    niri = {
-      enable = true;
-
-      # Temp workaround for libdisplay-info dependency issue
-      package = options.programs.niri.package.default.override {
-        libdisplay-info = pkgs.libdisplay-info.overrideAttrs (finalAttrs: {
-          version = "0.3.0";
-          src = pkgs.fetchFromGitLab {
-            domain = "gitlab.freedesktop.org";
-            owner = "emersion";
-            repo = "libdisplay-info";
-            rev = finalAttrs.version;
-            hash = "sha256-nXf2KGovNKvcchlHlzKBkAOeySMJXgxMpbi5z9gLrdc=";
-          };
-        });
-      };
-    };
-
+    niri.enable = true;
     dms-shell.enable = true;
   };
 
